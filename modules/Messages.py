6b4fc7383dfc25e3be4a6cdc43a6eadddb20e6a7
Ollegorii <remizov2003@gmail.com>
2025-04-07 23:37:18 +0300

    add radars messages

--- modules/Messages.py
+++ modules/Messages.py
@@ -2,6 +2,7 @@ from .BaseMessage import BaseMessage
 import numpy as np
 from .constants import MessageType
 from enum import Enum
+from typing import List
 
 from .BaseModel import BaseModel
 from .AirObject import AirObject
@@ -24,3 +25,10 @@ class LaunchedMissileMessage(BaseMessage):
     def __init__(self, time: int, sender_id: int, receiver_id: int, missile_id: int):
         super().__init__(type=MessageType.LAUNCHED_MISSILE, time=time, sender_id=sender_id, receiver_id=receiver_id)
         self.missile_id = missile_id
+
+
+class FoundObjectsMessage(BaseMessage):
+    '''Сообщение о найденных объектах радиолокатором'''
+    def __init__(self, time: int, sender_id: int, receiver_id: int, visible_objects: List[np.array]):
+        super().__init__(type=MessageType.FOUND_OBJECTS, time=time, sender_id=sender_id, receiver_id=receiver_id)
+        self.visible_objects = visible_objects
