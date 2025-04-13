188c0281c4848bd902816d3e1996a3a6e9b8f34d
Ollegorii <remizov2003@gmail.com>
2025-04-13 18:27:24 +0300

    solve conflict

--- UI/ObjectDialog.py
+++ UI/ObjectDialog.py
@@ -1,6 +1,6 @@
 from PyQt5.QtWidgets import (QComboBox, QDialog, QDialogButtonBox, QFormLayout, QLineEdit)
 
-from UI.Enums import ObjectType
+from Enums import ObjectType
 
 
 class ObjectDialog(QDialog):
